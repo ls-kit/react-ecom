72dc0f5b9acdf4a1d75e1aac39004d0196d36b19
ARahmanLskit <arahman@lskit.com>
2023-07-24 16:24:42 +0600

    save data to localstorage

--- src/components/HomePage/Registration.jsx
+++ src/components/HomePage/Registration.jsx
@@ -1,71 +1,76 @@
-import { useRef } from "react"
-import { Link } from "react-router-dom"
+import { useRef } from "react";
+import { Link } from "react-router-dom";
 
 export default function Registration() {
-    const nameRef = useRef()
-    const emailRef = useRef()
-    const passwordRef = useRef()
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
 
+  const handleRegistration = (e) => {
+    e.preventDefault();
+    const name = nameRef.current.value;
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    const registrationData = { name, email, password };
+    console.log(registrationData);
 
-    const handleRegistration = (e)=>{
-        e.preventDefault()
-        const name=nameRef.current.value;
-        const email = emailRef.current.value;
-        const password = passwordRef.current.value;
-        const registrationData = {name,email,password}
-        console.log(registrationData)
-        
-    }
-
-
-
-
-
+    // Save registration data to local storage
+    localStorage.setItem("registrationData", JSON.stringify(registrationData));
+  };
 
   return (
     <div>
-        <section className="contact_section ">
-    <div className="container px-0">
-      <div className="heading_container ">
-        <h2 className="">
-          Registration
-        </h2>
-      </div>
-    </div>
-    <div className="container container-bg">
-      <div className="row">
-        <div className="col-lg-7 col-md-6 px-0">
-          <div className="map_container">
-            <div className="map-responsive">
-              <img src="https://img.freepik.com/free-vector/sign-concept-illustration_114360-5425.jpg?w=2000" alt="" width={'100%'} />
+      <section className="contact_section ">
+        <div className="container px-0">
+          <div className="heading_container ">
+            <h2 className="">Registration</h2>
+          </div>
+        </div>
+        <div className="container container-bg">
+          <div className="row">
+            <div className="col-lg-7 col-md-6 px-0">
+              <div className="map_container">
+                <div className="map-responsive">
+                  <img
+                    src="https://img.freepik.com/free-vector/sign-concept-illustration_114360-5425.jpg?w=2000"
+                    alt=""
+                    width={"100%"}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 col-lg-5 px-0">
+              <form onSubmit={handleRegistration}>
+                <div>
+                  <input type="text" placeholder="Name" ref={nameRef} />
+                </div>
+                <div>
+                  <input type="email" placeholder="Email" ref={emailRef} />
+                </div>
+                <div>
+                  <input
+                    type="password"
+                    placeholder="******"
+                    ref={passwordRef}
+                  />
+                </div>
+                {/* <div>
+              <input type="text" className="message-box" placeholder="Message" />
+            </div> */}
+                <h6>
+                  Already have an account? please{" "}
+                  <Link to={"/login"} className="text-success">
+                    Login
+                  </Link>
+                </h6>
+                <div className="d-flex ">
+                  <button type="submit">SEND</button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
-        <div className="col-md-6 col-lg-5 px-0">
-          <form onSubmit={handleRegistration}>
-            <div>
-              <input type="text" placeholder="Name" ref={nameRef} />
-            </div>
-            <div>
-              <input type="email" placeholder="Email" ref={emailRef} />
-            </div>
-            <div>
-              <input type="password" placeholder="******" ref={passwordRef} />
-            </div>
-            {/* <div>
-              <input type="text" className="message-box" placeholder="Message" />
-            </div> */}
-            <h6>Already have an account? please <Link to={'/login'} className="text-success">Login</Link></h6>
-            <div className="d-flex ">
-              <button type="submit">
-                SEND
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
+      </section>
     </div>
-  </section>
-    </div>
-  )
+  );
 }
